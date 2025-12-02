8e56b72010e2e8b0976182255e2610e2d1eea379
Ben Aguirre <baguirre@luc.edu>
2025-12-02 13:21:50 -0600

    day 2

--- day_2/part_2.py
+++ day_2/part_2.py
@@ -1,21 +1,23 @@
+import time
+
+
 def solve(inp):
     res = 0
     with open(inp, "r") as file:
         lines = file.read().replace("\n", "").split(",")
-
     for line in lines:
         arr = line.split("-")
         start, end = int(arr[0]), int(arr[1])
         for i in range(start, end + 1):
             num = str(i)
-            flag = False
             half = len(num) // 2
             for j in range(1, half + 1):
+                if len(num) % j != 0:
+                    continue
                 t = [num[k : k + j] for k in range(0, len(num), j)]
                 if all(n == t[0] for n in t):
-                    flag = True
+                    res += i
                     break
-            res += i if flag else 0
     return res
 
 
