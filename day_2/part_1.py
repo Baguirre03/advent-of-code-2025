dca2049c236dd537c204fe0a15da619afe3b08b9
Ben Aguirre <baguirre@luc.edu>
2025-12-02 13:27:47 -0600

    day 2

--- day_2/part_1.py
+++ day_2/part_1.py
@@ -8,12 +8,13 @@ def solve(inp):
         start, end = int(arr[0]), int(arr[1])
         for i in range(start, end + 1):
             num = str(i)
-            if not len(num) // 2:
+            n = len(num)
+            if n % 2:
                 continue
-            half = len(num) // 2
+            half = n // 2
             res += i if num[:half] == num[half:] else 0
     return res
 
 
-print(solve("example.txt"))
+# print(solve("example.txt"))
 print(solve("input.txt"))
