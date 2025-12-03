73beef5a02c411f8ac3939fd4577bb44cdcbfe9f
Ben Aguirre <baguirre@luc.edu>
2025-12-03 15:03:45 -0600

    day 3

--- day_3/part_2.py
+++ day_3/part_2.py
@@ -3,21 +3,20 @@ def solve(inp):
     with open(inp, "r") as file:
         lines = file.readlines()
         for l in lines:
-            rows.append(l.replace("\n", ""))
+            rows.append(list(map(lambda x: int(x), l.replace("\n", ""))))
 
     res = 0
     for row in rows:
-        r = list(map(lambda x: int(x), list(row)))
-        search = len(r) - 12
+        search = len(row) - 12
         last_indx = -1
         arr = []
         for _ in range(12):
             add = (0, -1)
             for j in range(last_indx + 1, last_indx + search + 1):
-                add = max(add, (r[j], j), key=lambda x: (x[0], -x[1]))
+                add = max(add, (row[j], j), key=lambda x: (x[0], -x[1]))
             arr.append(str(add[0]))
             last_indx = add[1]
-            search = len(r) - last_indx - 12 + len(arr)
+            search = len(row) - last_indx - 12 + len(arr)
         res += int("".join(arr))
 
     return res
