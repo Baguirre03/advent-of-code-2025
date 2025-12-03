a118ca5cf5369785e728495dd3a02a7c2fe61cc5
Ben Aguirre <baguirre@luc.edu>
2025-12-03 09:34:57 -0600

    day 3

--- day_3/part_2.py
+++ day_3/part_2.py
@@ -8,21 +8,17 @@ def solve(inp):
     res = 0
     for row in rows:
         r = list(map(lambda x: int(x), list(row)))
-        length = len(r)
-        search = length - 12
+        search = len(r) - 12
         last_indx = -1
         arr = []
         for _ in range(12):
             add = (0, -1)
-            indexes = []
             for j in range(last_indx + 1, last_indx + search + 1):
-                indexes.append(j)
                 add = max(add, (r[j], j), key=lambda x: (x[0], -x[1]))
-            arr.append(add[0])
+            arr.append(str(add[0]))
             last_indx = add[1]
-            search = length - (last_indx) - 12 + len(arr)
-            search = max(1, search)
-        res += int("".join([str(n) for n in arr]))
+            search = len(r) - last_indx - 12 + len(arr)
+        res += int("".join(arr))
 
     return res
 
