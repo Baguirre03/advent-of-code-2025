8c9c84c1367e6a8a52fd27fd35100de3480a9aaf
Ben Aguirre <baguirre@luc.edu>
2025-12-03 15:04:30 -0600

    day 3

--- day_3/part_1.py
+++ day_3/part_1.py
@@ -3,16 +3,15 @@ def solve(inp):
     with open(inp, "r") as file:
         lines = file.readlines()
         for l in lines:
-            rows.append(l.replace("\n", ""))
+            rows.append(list(map(lambda x: int(x), l.replace("\n", ""))))
 
     res = 0
     for row in rows:
-        r = list(map(lambda x: int(x), list(row)))
         l1, l2 = (0, -1), (0, -1)
-        for i in range(len(r) - 1):
-            l1 = max(l1, (r[i], i), key=lambda x: (x[0], -x[1]))
-        for i in range(len(r) - 1, l1[1], -1):
-            l2 = max(l2, (r[i], i))
+        for i in range(len(row) - 1):
+            l1 = max(l1, (row[i], i), key=lambda x: (x[0], -x[1]))
+        for i in range(len(row) - 1, l1[1], -1):
+            l2 = max(l2, (row[i], i))
         res += int(f"{l1[0]}{l2[0]}")
     return res
 
